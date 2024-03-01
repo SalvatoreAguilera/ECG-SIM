248a83e06186114fe3257dc813aa044a05551c95
SalvatoreAguilera <sgutierrez49@csub.edu>
2024-02-29 17:32:06 -0800

    aniamtion

--- coor.py
+++ coor.py
@@ -8,19 +8,24 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Set threshold level
 threshold_level = 50
 
-# Find coordinates of all pixels below threshold
-coords = np.column_stack(np.where(gray < threshold_level))
-
-print(coords)
-
 # Create mask of all pixels lower than threshold level
 mask = gray < threshold_level
 
 # Color the pixels in the mask
-image[mask] = (204, 119, 0)
+image[mask] = (255, 255, 255)
+image[~mask] = (0,0,0)
 
-cv2.imshow('image', image)
-cv2.waitKey()
+white_pixels = np.where(image == 255)
+animated_image = np.zeros_like(image)
+chunk_size = 100
 
-def main():
-    window 
+
+for i in range(0, len(white_pixels[0]), chunk_size):  # Iterate over rows
+    chunk_rows = white_pixels[0][i:i+chunk_size]
+    chunk_cols = white_pixels[1][i:i+chunk_size]
+    for y, x in zip(chunk_rows, chunk_cols):  # Combine and iterate over coordinates
+        if 0 <= x < animated_image.shape[1] and 0 <= y < animated_image.shape[0]:
+            animated_image[y, x] = 255  # Set selected pixels to white
+    animated_image = cv2.convertScaleAbs(animated_image, alpha=(255.0/animated_image.max()))
+    cv2.imshow('image', animated_image)
+    cv2.waitKey(1)
