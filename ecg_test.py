703291705ff042c125078489d574c18b94eed30c
raulVer15 <111897909+raulVer15@users.noreply.github.com>
2024-03-16 19:54:37 -0700

    animation

--- ecg_test.py
+++ ecg_test.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 import cv2
 import matplotlib.pylab as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg) 
 import time
 from tkinter import *
 from glob import glob
-import coor
+
 root = Tk() 
   
-root.geometry( "300x300" ) 
+root.geometry( "500x300" ) 
   
 def newWindow(): 
     window = Toplevel(root)
@@ -17,10 +19,12 @@ def newWindow():
 
     ecg_list = [["Atrial Flutter", 0],["VF", 1],["Atrial Tachycardia",2],["SVT",3],["A Fib",4],["Normal Sinus Rhythms", 5],["VT", 6],["Multifocal Atrial Tachycardia",7],
                 ["TdP",8], ["Bradycardia",9], ["Tachycardia", 10]]
-    
+    print(clicked.get())
     for i in range(0,len(ecg_list)):
         if ( clicked.get() == ecg_list[i][0]):
             ecg_num = ecg_list[i][1]
+        else:
+            ecg_num = ecg_list[5][1]
 
     print(clicked.get())
     window.configure( background='black' )
@@ -31,47 +35,42 @@ def newWindow():
     print(len(ecg_files))
     
     img_cv2 = cv2.imread(ecg_files[ecg_num])                           # reads one ecg
-    kernal_edge = np.array([[-1,-1,-1],                          # kernal used for edge detection
-                            [-1,8,-1],
-                            [-1,-1,-1]])
-    edge_img_cv2 = cv2.filter2D(img_cv2, -1, kernal_edge)        # filters with the kernal
 
-    fig, axs = plt.subplots(1,2, figsize=(10,5))                       # figure to show img with plt
-    axs[0].imshow(edge_img_cv2)
-    axs[1].imshow(img_cv2)
-    axs[0].axis("off")
-    axs[1].axis("off")
+    fig, axs = plt.subplots(figsize=(10,5))                       # figure to show img with plt
+    axs.imshow(img_cv2)
+    axs.axis("off")
     plt.show()
 
-    xinc = 5
-    yinc = 0
-    create_canvas_animation = canvas_animation(window)
-    ecg_signal_animation(window, create_canvas_animation, xinc,yinc)
+# equation
+    t = np.linspace(0,10,100)
+    y= np.sin(t)
 
+    # create figure 
+    fig, axis = plt.subplots(facecolor="black")                   # background of fig black
 
-def canvas_animation(window):
-     # creating start line
-    C = Canvas(window, bg="black", width=800, height=200)
-    line = C.create_line(0, 100, 800,100 , fill= "#76EE00", width=2)
-    C.pack(fill="both", expand=True)
-    return C
+    axis.set_xlim([min(t),max(t)])                                # set the limits of time for x
+    axis.set_ylim([-2,2])                                         # Set limit for amplitude for y
 
-def ecg_signal_animation(window, C, xinc, yinc):
-    signal = C.create_line(0, 100, 10, 100, fill= "black", width=2)
-    # loop
-    index = 0
+    animated_plot, = axis.plot([],[])                             # empty plot to set the x and y from the equation
+
+    axis.set_facecolor("black")                                   # set background of graph as black
     
-    while True:
-        C.move(signal, xinc,yinc)
-        window.update()
-        time.sleep(0.01)
-        signal_pos = C.coords(signal)
-        xl,yl,xr,yr = signal_pos
-        if xl < abs(xinc) or xr >= 800-abs(xinc):
-            xl,yl,xr,yr = 0,0,0,0
-            C.delete(signal)
-            signal = C.create_line(0, 100, 10, 100, fill= "black", width=2)
-                
+    def update(frame):
+
+        # update the new points after each frame
+        animated_plot.set_data(t[:frame], y[:frame])
+
+        return animated_plot
+    
+    # call animation
+    animate = FuncAnimation(fig= fig, func= update,frames=len(t),interval=25)
+
+    # Embed fig in canvas
+    canvas = FigureCanvasTkAgg(fig, master=window)  
+
+    # draw ecg signal
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=TOP,fill = BOTH, expand= 1)
   
 options = [ 
     "Normal Sinus Rhythms",
@@ -96,9 +95,13 @@ drop.pack()
   
 button = Button( root , text = " Start " , command = newWindow ).pack() 
 
-  
-  
+# button to quit
+button = Button(master=root, text="Quit", command=quit)
+button.pack(side= BOTTOM)
+
 root.mainloop()
+
+
 
 
 
