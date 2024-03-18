edab8b1533c7e63c904c505be66a1113acd5257e
raulVer15 <111897909+raulVer15@users.noreply.github.com>
2024-03-17 23:36:21 -0700

    Update ecg_test.py

--- ecg_test.py
+++ ecg_test.py
@@ -4,14 +4,44 @@ import cv2
 import matplotlib.pylab as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg) 
-import time
+from PIL import ImageTk, Image
 from tkinter import *
 from glob import glob
 
-root = Tk() 
-  
-root.geometry( "500x300" ) 
-  
+ # new Window Function
+
+def animation(window):
+    t = np.linspace(0,10,100)
+    y= np.sin(t)
+
+    # create figure 
+    fig, axis = plt.subplots(facecolor="black")                   # background of fig black
+
+    axis.set_xlim([min(t),max(t)])                                # set the limits of time for x
+    axis.set_ylim([-2,2])                                         # Set limit for amplitude for y
+
+    animated_plot, = axis.plot([],[], color="green")              # empty plot to set the x and y from the equation
+
+    axis.set_facecolor("black")                                   # set background of graph as black
+
+    def update(frame):
+
+        # update the new points after each frame
+        animated_plot.set_data(t[:frame], y[:frame])
+
+        return animated_plot
+    
+    # call animation
+    animate = FuncAnimation(fig= fig, func= update,frames=len(t),interval=25, repeat=False)
+
+    # Embed fig in canvas
+    canvas = FigureCanvasTkAgg(fig, master=window)  
+
+    # draw ecg signal
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=TOP,fill = BOTH, expand= 1)
+
+
 def newWindow(): 
     window = Toplevel(root)
     window.geometry( "800x200" )
@@ -23,8 +53,7 @@ def newWindow():
     for i in range(0,len(ecg_list)):
         if ( clicked.get() == ecg_list[i][0]):
             ecg_num = ecg_list[i][1]
-        else:
-            ecg_num = ecg_list[5][1]
+
 
     print(clicked.get())
     window.configure( background='black' )
@@ -41,37 +70,29 @@ def newWindow():
     axs.axis("off")
     plt.show()
 
-# equation
-    t = np.linspace(0,10,100)
-    y= np.sin(t)
+    # call animation function
+    live_signal = animation(window)   
 
-    # create figure 
-    fig, axis = plt.subplots(facecolor="black")                   # background of fig black
-
-    axis.set_xlim([min(t),max(t)])                                # set the limits of time for x
-    axis.set_ylim([-2,2])                                         # Set limit for amplitude for y
-
-    animated_plot, = axis.plot([],[])                             # empty plot to set the x and y from the equation
-
-    axis.set_facecolor("black")                                   # set background of graph as black
+# main loop
     
-    def update(frame):
-
-        # update the new points after each frame
-        animated_plot.set_data(t[:frame], y[:frame])
-
-        return animated_plot
-    
-    # call animation
-    animate = FuncAnimation(fig= fig, func= update,frames=len(t),interval=25)
-
-    # Embed fig in canvas
-    canvas = FigureCanvasTkAgg(fig, master=window)  
-
-    # draw ecg signal
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=TOP,fill = BOTH, expand= 1)
+root = Tk() 
   
+root.geometry( "500x300" ) 
+root.title("ECG Simulator")                       # set title of root window
+
+
+Label(root, text="Electrocardiogram Simulator", font=("Arial", 20)).pack(side=TOP, pady= 10)
+
+# left frame ECG chart display
+left_frame = LabelFrame(text="ECG Chart", foreground="black", font=("Arial", 20),background= "white", height=500, width=500)
+img = ImageTk.PhotoImage(file=("./ECG/normal-.jpeg"))
+
+left_frame.pack(side=LEFT, expand= YES, padx=10, pady=10, anchor=W)
+left_label = Label(left_frame,image=img, height=400, width=300).pack(anchor=CENTER)
+
+right_frame= LabelFrame().pack(fill=BOTH,side=RIGHT, expand= NO, anchor=CENTER)
+
+# Options for disease 
 options = [ 
     "Normal Sinus Rhythms",
     "Bradycardia", 
@@ -85,22 +106,20 @@ options = [
     "VF",
     "TdP"
 ] 
-  
-clicked = StringVar() 
-  
+# menu for disease 
+clicked = StringVar()  
 clicked.set( " Heart Rhytms " ) 
-  
-drop = OptionMenu( root , clicked , *options ) 
+drop = OptionMenu( right_frame , clicked , *options ) 
 drop.pack() 
-  
-button = Button( root , text = " Start " , command = newWindow ).pack() 
+
+# Button to start signal
+button = Button( right_frame , text = " Start " , command = newWindow ).pack() 
 
 # button to quit
-button = Button(master=root, text="Quit", command=quit)
-button.pack(side= BOTTOM)
+button = Button(root, text="Quit", command=quit)
+button.pack(side=BOTTOM)
 
 root.mainloop()
-
 
 
 
